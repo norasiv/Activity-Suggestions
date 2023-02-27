56f4a29e495a2624e0c14e05cf44cd622a5101e9
norasiv <norasiv@hotmail.com>
2023-02-27 13:01:26 +0100

    updated animation

--- ActivitySuggestion/Controllers/ActivityDetailController.swift
+++ ActivitySuggestion/Controllers/ActivityDetailController.swift
@@ -13,9 +13,8 @@ class ActivityDetailController: UIViewController {
     var date = ""
     var price = 0.0
     
-    let emojis = [0.0: "💯",
-                  0.1 : "✅"
-    ] as [AnyHashable : String]
+    var emojis = ""
+
     
     
     @IBOutlet weak var activityLabel: UILabel!
@@ -31,28 +30,37 @@ class ActivityDetailController: UIViewController {
         
     }
     
-    
-    func getEmoji() {
-        let numberOfEmoji = 20
-        for _ in 1...numberOfEmoji {
-            let emojiLabel = UILabel()
-            let randomXPosition = Int(arc4random_uniform(300) + 1)
-            emojiLabel.frame = CGRect(x: randomXPosition, y: 0, width: 200, height: 100)
-            
-            for (activtiyPrice, emoji) in emojis {
-                if (price == activtiyPrice as! Double){
-                    emojiLabel.text = emoji
-                    self.view.addSubview(emojiLabel)
-                    
-                    rainAnimation(label: emojiLabel, randomXPosition: randomXPosition)
-                }
-            }
+    // check price and give correct emoji
+    func getPrice(){
+        if price == 0.0 {
+            emojis = "💯"
+        } else {
+            emojis = "✅"
         }
     }
     
-    func rainAnimation(label: UILabel, randomXPosition: Int){
-        UILabel.animate(withDuration: 5, delay: 0.5, options: [.curveLinear], animations: {
-            label.frame = CGRect(x: randomXPosition, y: 0, width: 200, height: 2000)
+    
+    // get emoji and make subview for animation
+    func getEmoji() {
+        getPrice()
+        let numberOfEmoji = 15
+        for _ in 1...numberOfEmoji {
+            let emojiLabel = UILabel()
+            let randomDelay = CGFloat.random(in: 0..<10) / 5
+            let randomXPosition = Int(arc4random_uniform(350) + 1)
+            emojiLabel.frame = CGRect(x: randomXPosition, y: -100, width: 200, height: 100)
+            emojiLabel.text = emojis
+            
+            self.view.addSubview(emojiLabel)
+                    
+            rainAnimation(label: emojiLabel, randomXPosition: randomXPosition, delay: randomDelay)
+        }
+    }
+    
+    // animation -- see reference 2
+    func rainAnimation(label: UILabel, randomXPosition: Int, delay: CGFloat){
+        UILabel.animate(withDuration: 5, delay: delay, options: [.curveLinear, .repeat], animations: {
+            label.frame = CGRect(x: randomXPosition, y: -100, width: 200, height: 2000)
         }, completion: nil)
     }
     
